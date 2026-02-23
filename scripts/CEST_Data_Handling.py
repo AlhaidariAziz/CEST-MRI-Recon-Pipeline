a44491c1c255009f2465789f76205226f7584589
Abdulaziz Alhaidari <iwbi112h@mailhub.rrze.uni-erlangen.de>
2026-02-23 23:37:49 +0100

    update argparse and structure to accomodate for seperate ACS measurement

--- scripts/CEST_Data_Handling.py
+++ scripts/CEST_Data_Handling.py
@@ -17,45 +17,49 @@ import os
 import pathlib
 import torch
 import twixtools
-
+import argparse 
 import numpy as np
 import sigpy as sp
 
 from sigpy.mri import app, epi, sms, cc
 
+parser=argparse.ArgumentParser(description='Extract data from Twix file to h5py')
+parser.add_argument('--data', default=None, help='data file path')
+parser.add_argument('--acs', type=bool, default=False, help='let the code know if it for ACS data')
+
+args=parser.parse_args()
+
 
 # print(np.__file__)
 # print(torch.__file__)
 # print(sp.__file__)
+if args.data is None:
+    # DIR = os.path.dirname(os.path.realpath(__file__))
+    DIR='/home/vault/iwbi/iwbi112h/CEST_DATA/'
 
-# DIR = os.path.dirname(os.path.realpath(__file__))
-DIR='/home/vault/iwbi/iwbi112h/CEST_DATA'
-outprefstr = '/home/vault/iwbi/iwbi112h/CEST_DATA/'
+    #2D Grappa single offset (WM)
+    # infile= 'meas_MID00454_FID01852_100_wip_Snap_Cai_4_3_1_1_2.dat'
 
-#2D Grappa single offset (WM)
-# infile= 'meas_MID00454_FID01852_100_wip_Snap_Cai_4_3_1_1_2.dat'
+    # 2D Grappa 65 offsets (WM)
+    # infile='meas_MID00455_FID01853_100_wip_Snap_Cai_4_3_1_1_2_1p00.dat'
+    # 1D Grappa single offset (WASABI)
+    # infile='meas_MID00018_FID13340_gre_cest_100_p145_ice_001.dat'
 
-# 2D Grappa 65 offsets (WM)
-# infile='meas_MID00455_FID01853_100_wip_Snap_Cai_4_3_1_1_2_1p00.dat'
-# 1D Grappa single offset (WASABI)
-# infile='meas_MID00018_FID13340_gre_cest_100_p145_ice_001.dat'
-
-# 1D Grappa 35 offset  (WASABI)
-# infile='meas_MID00022_FID13344_gre_cest_WASABI_32.dat'
+    # 1D Grappa 35 offset  (WASABI)
+    # infile='meas_MID00022_FID13344_gre_cest_WASABI_32.dat'
 
 
-# CEST_34 offsets (Simon)
-# infile='meas_MID00070_FID33988_CEST_3shot.dat'  #3shot
-infile='meas_MID00071_FID33989_CEST_1shot.dat'  #1shot
+    # CEST_34 offsets (Simon)
+    # infile ='meas_MID00070_FID33988_CEST_3shot.dat'  #3shot
+    # infile ='meas_MID00071_FID33989_CEST_1shot.dat'  #1shot
+    infile = 'acs_1shot.dat' # ACS
 
-# %% argument parser
-parser = argparse.ArgumentParser(description='prepare data and store output k-space data separately.')
+    file_dir=DIR+infile
+    out_dir = DIR
+else:
+    file_dir=args.data
+    out_dir=np.__file__
 
-parser.add_argument('--data',
-                    default= infile,
-                    help='raw dat file.')
-
-args = parser.parse_args()
 
 
 if torch.cuda.is_available():
@@ -66,17 +70,15 @@ else:
 print('> device: ', device)
 
 # %% prepare output string
-print('>>> dat: ', args.data)
+print('>>> .dat: ', file_dir)
 
-instr = DIR + '/' + args.data
-# outprefstr = instr.split('.dat')[0]
 
-print('> output prefix: ', outprefstr)
+print('> output path: ', out_dir)
 # make a new directory if not exist
-# pathlib.Path(outprefstr).mkdir(parents=True, exist_ok=True)
+# pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
 
 # %% read in twix data
-twixobj = twixtools.read_twix(instr)
+twixobj = twixtools.read_twix(file_dir)
 
 twix = twixobj[-1]
 
@@ -159,25 +161,39 @@ print('> N_Partitions ', int(N_Part))
 
 kdat_twix = np.squeeze(kdat_twix[:])
 print('> unsorted squeezed kdat shape: ', kdat_twix.shape)
-kdat_twix = np.transpose(kdat_twix, (-5,-2,-4,-3,-1)) #choose the order that suituble for your application. 
+
+if args.acs is False:
+    kdat_twix = np.transpose(kdat_twix, (-5,-2,-4,-3,-1)) #choose the order that suituble for your application. 
+    file_name = f'/kdat_3D_R{int(Reps)}_C{int(N_coil)}_'+ infile.split('_')[-1].rsplit('.',1)[0]+'.h5'
+else:
+    kdat_twix = np.transpose(kdat_twix, (-2,-4,-3,-1)) #choose the order that suituble for your ACS. 
+    file_name = f'/ACS_3D_C{int(N_coil)}_'+ infile.split('_')[-1].rsplit('.',1)[0]+'.h5'
 
 print('> sorted kdat shape: ', kdat_twix.shape)
 # raise SystemExit('Stop here for now')
 
-file_name = f'/kdat_3D_R{int(Reps)}_C{int(N_coil)}_'+ infile.split('_')[-1].rsplit('.',1)[0]+'.h5'
-with h5py.File(outprefstr + file_name, 'w') as f:
+
+with h5py.File(out_dir + file_name, 'w') as f:
     # split CEST Data into 2 parts for easier handling, it can be split into more parts if needed based on Reps size. 
-
-    dset=f.create_dataset('kdat_01', data = kdat_twix[0:17,...])
-    dset.attrs['Accel_PE1'] = Accel_PE1
-    dset.attrs['Accel_3D'] = Accel_3D
-    dset.attrs['Accel_total'] = Accel_total
-    dset.attrs['central_Lin'] = central_Lin
-    dset.attrs['central_Part'] = central_part
-    f.create_dataset('kdat_02', data = kdat_twix[17::,...])
-    if Accel_total > 1:
-        f.create_dataset('ref', data = refs)
-    f.close()
-
-print ('Done, saved  : ', outprefstr + file_name)
+    if args.acs is False:
+        dset=f.create_dataset('kdat_01', data = kdat_twix[0:17,...])
+        dset.attrs['Accel_PE1'] = Accel_PE1
+        dset.attrs['Accel_3D'] = Accel_3D
+        dset.attrs['Accel_total'] = Accel_total
+        dset.attrs['central_Lin'] = central_Lin
+        dset.attrs['central_Part'] = central_part
+        f.create_dataset('kdat_02', data = kdat_twix[17::,...])
+        if Accel_total > 1:
+            f.create_dataset('ref', data = refs)
+        f.close()
+    else:
+        dset=f.create_dataset('refs', data = kdat_twix[:])
+        dset.attrs['Accel_PE1'] = Accel_PE1
+        dset.attrs['Accel_3D'] = Accel_3D
+        dset.attrs['Accel_total'] = Accel_total
+        dset.attrs['central_Lin'] = central_Lin
+        dset.attrs['central_Part'] = central_part
+        f.close()
+        
+print ('Done, saved  : ', out_dir + file_name)
 # %%
